bbe62d660d1fcf9374cde472a805933a71d5b172
Ricardo Perdiz <ricoperdiz@gmail.com>
2021-01-31 01:12:57 -0400

    remove codigo

--- codigo/configuracoes.R
+++
@@ -1,6 +0,0 @@
-##################
-## Prepara .gitignore
-##################
-library(gitignore)
-new_lines <- gi_fetch_templates("R")
-gi_write_gitignore(fetched_template = new_lines)
--- codigo/setup.R
+++
@@ -1,9 +0,0 @@
-# Setup
-library("usethis")
-
-# Readme.Rmd
-# use_readme_rmd()
-
-#  add in the github action for our book to automatically add documentation. 
-# usethis::use_github_actions()
-# usethis::use_github_action(url = "https://raw.githubusercontent.com/r-lib/actions/master/examples/pkgdown.yaml")
